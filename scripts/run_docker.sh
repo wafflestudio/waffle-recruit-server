ba8c1f049dd08b9cb62ae79e2ff4f661a2fe1886
daeyong <iwin1203@naver.com>
2022-07-28 00:50:36 +0900

    feat/docker 스크립트 쪼꼼 변경

--- scripts/run_docker.sh
+++ scripts/run_docker.sh
@@ -1,2 +1,12 @@
-sudo docker create -it --network none --name test whizkyu/recruit:1.1 > /dev/null
-sudo docker start test > /dev/null
+sudo docker create -it --network none --name test1 whizkyu/recruit:1.1 > /dev/null
+sudo docker create -it --network none --name test2 whizkyu/recruit:1.1 > /dev/null
+sudo docker create -it --network none --name test3 whizkyu/recruit:1.1 > /dev/null
+sudo docker create -it --network none --name test4 whizkyu/recruit:1.1 > /dev/null
+sudo docker create -it --network none --name test5 whizkyu/recruit:1.1 > /dev/null
+sudo docker start test1 > /dev/null
+sudo docker start test2 > /dev/null
+sudo docker start test3 > /dev/null
+sudo docker start test4 > /dev/null
+sudo docker start test5 > /dev/null
+
+# https://kimjingo.tistory.com/60 -> cpu 자원 제어 가능
