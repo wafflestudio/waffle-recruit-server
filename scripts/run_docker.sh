e768f991afcdbbcbc31372a20ceedd35fb1be105
ec2-deploy <iwin1203@naver.com>
2022-08-02 13:12:38 +0000

    스크립트

--- scripts/run_docker.sh
+++ scripts/run_docker.sh
@@ -3,10 +3,16 @@ sudo docker create -it --network none --name test2 whizkyu/recruit:1.1 > /dev/nu
 sudo docker create -it --network none --name test3 whizkyu/recruit:1.1 > /dev/null
 sudo docker create -it --network none --name test4 whizkyu/recruit:1.1 > /dev/null
 sudo docker create -it --network none --name test5 whizkyu/recruit:1.1 > /dev/null
+sudo docker create -it --network none --name test6 whizkyu/recruit:1.1 > /dev/null
+sudo docker create -it --network none --name test7 whizkyu/recruit:1.1 > /dev/null
+sudo docker create -it --network none --name test8 whizkyu/recruit:1.1 > /dev/null
 sudo docker start test1 > /dev/null
 sudo docker start test2 > /dev/null
 sudo docker start test3 > /dev/null
 sudo docker start test4 > /dev/null
 sudo docker start test5 > /dev/null
+sudo docker start test6 > /dev/null
+sudo docker start test7 > /dev/null
+sudo docker start test8 > /dev/null
 
 # https://kimjingo.tistory.com/60 -> cpu 자원 제어 가능
