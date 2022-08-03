4440171d8eba69da4ffd715a4ef877455e9743b8
ec2-deploy <iwin1203@naver.com>
2022-08-03 14:19:30 +0000

    유저권한제한

--- scripts/add_user.sh
+++ scripts/add_user.sh
@@ -2,4 +2,5 @@ CREDENTIAL=$1
 CONTAINER_ID=$2
 sudo docker exec ${CONTAINER_ID} userdel -r ${CREDENTIAL}
 sudo docker exec ${CONTAINER_ID} useradd ${CREDENTIAL}
+sudo docker exec ${CONTAINER_ID} chmod 555 /home/${CREDENTIAL}
 sudo docker cp codes/${CREDENTIAL} ${CONTAINER_ID}:/home/
--- scripts/del_user.sh
+++ scripts/del_user.sh
@@ -1,5 +1,4 @@
 CREDENTIAL=$1
 CONTAINER_ID=$2
-echo "yahoo"
 sudo docker exec ${CONTAINER_ID} userdel ${CREDENTIAL} -r
 sudo docker exec ${CONTAINER_ID} rm -rf /home/${CREDENTIAL}
